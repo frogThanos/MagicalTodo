34e519fa7a5e7f5ed5521d21071c1c1b2258b824
frogThanos <thanos@afrogleap.com>
2019-08-09 22:32:59 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <Fragment>
+      <SafeAreaView>
+        <Text>MagicalTodo</Text>
+      </SafeAreaView>
+    </Fragment>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
